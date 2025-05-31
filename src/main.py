890bd23a2ec572fb7c3bfff96db3672b3bade9e5
adelsonsljunior <junioradelsonst@gmail.com>
2025-05-31 12:08:14 -0300

    refactor main.py for improved code organization

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 import os
-
 from dotenv import load_dotenv
+from fastapi import FastAPI
+import uvicorn
 
 from database import create_tables
-
-from fastapi import FastAPI
 from routes.post_router import router as post
+
+create_tables()
 
 app = FastAPI()
 app.include_router(post)
 
-create_tables()
-
-
 load_dotenv()
 PORT = int(os.getenv("PORT", 8080))
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT)
