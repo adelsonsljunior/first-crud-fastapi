e5d432f94cd2b312eff22d34634c625a8f43fdd2
adelsonsljunior <junioradelsonst@gmail.com>
2025-05-31 09:24:50 -0300

    add implement find_by_id endpoint in PostController

--- src/controllers/post_controller.py
+++ src/controllers/post_controller.py
@@ -1,7 +1,9 @@
-from models.post import Post
 from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
 
+from models.post import Post
 from schemas.post_schema import PostCreateDto
+from schemas.responses import ErrorResponse
 
 
 class PostController:
@@ -17,3 +19,12 @@ class PostController:
         self.db.commit()
         self.db.refresh(db_post)
         return db_post.id
+
+    def find_by_id(self, post_id: int):
+        post = self.db.query(Post).filter(Post.id == post_id).first()
+        if not post:
+            return JSONResponse(
+                status_code=404,
+                content=ErrorResponse(message="Post não encontrado").model_dump(),
+            )
+        return post
--- src/routes/post_router.py
+++ src/routes/post_router.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from database import get_db
 
 from typing import List
 
+from database import get_db
 from controllers.post_controller import PostController
-from schemas.post_schema import PostCreateDto, PostResponse, IdResponse
+from schemas.post_schema import PostCreateDto, PostResponse
+from schemas.responses import IdResponse, ErrorResponse
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
@@ -20,3 +21,11 @@ async def find_all(db: Session = Depends(get_db)):
 async def create(post: PostCreateDto, db: Session = Depends(get_db)):
     controller = PostController(db)
     return controller.create(post)
+
+
+@router.get(
+    "/{post_id}", response_model=PostResponse, responses={404: {"model": ErrorResponse}}
+)
+async def find_by_id(post_id: int, db: Session = Depends(get_db)):
+    controller = PostController(db)
+    return controller.find_by_id(post_id)
