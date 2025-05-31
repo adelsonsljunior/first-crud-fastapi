08fe4468c087c7360ecdd22d29d6935cbb916d11
adelsonsljunior <junioradelsonst@gmail.com>
2025-05-31 08:46:40 -0300

    add Post model

---
+++ src/models/post.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+from database import Base
+
+
+class Post(Base):
+    __tablename__ = "post"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    body = Column(String, nullable=False)
+    archived = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
