6a330817350eef467a20d4a5a139fbb3d324b4d4
adelsonsljunior <junioradelsonst@gmail.com>
2025-06-02 10:35:36 -0300

    add validation to body and username fields

--- src/schemas/post_schema.py
+++ src/schemas/post_schema.py
@@ -1,13 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 
 
 class PostBase(BaseModel):
-    body: str
+    body: str = Field(
+        min_length=1,
+        max_length=255,
+        description="O body deve ter entre 1 e 255 caracteres",
+    )
 
 
 class PostCreateDto(PostBase):
-    username: str | None
+    username: str = Field(
+        min_length=3,
+        max_length=50,
+        description="O username deve ter entre 3 e 50 caracteres",
+    )
 
 
 class PostUpdateDto(PostBase):
