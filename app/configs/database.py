aaf1bca9526c4e7e35f05fbaea8178e6673587e7
adelsonsljunior <junioradelsonst@gmail.com>
2025-12-15 18:09:38 -0300

    refactor: Formatting the DB_URL environment within the application with individual envs

--- app/configs/database.py
+++ app/configs/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from app.configs.env import DB_URL
+from app.configs.env import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
 
+
+DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(
     DB_URL,
--- app/configs/env.py
+++ app/configs/env.py
@@ -5,4 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_URL = os.getenv("DB_URL")
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
